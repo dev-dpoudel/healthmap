f745bf992b2b424e082aa26e638364cf51dd3dcc
dev-dpoudel <diensh2019@cdcsit.edu.np>
2020-12-29 18:31:59 +0545

    Added model definitions for Forums and FileManager modules

--- filemanager/models.py
+++ filemanager/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+# Return full file_path for given files
+def file_path(table, field, type, id, name, ext):
+    return '{0}/{1}/{2}/{3}/{4}.{5}'.format(table, field, type, id, name, ext)
+
+
+# Definition of File Manager
+class FileManger(models.Model):
+    ''' Class Modal for File manager. '''
+
+    file_id = models.AutoField(primary_key=True, unique=True)
+    ref_id = models.IntegerField()
+    ref_field = models.CharField()
+    ref_table = models.CharField()
+    file_type = models.CharField(max_length=10)
+    file_name = models.CharField(max_length=50)
+    file_ext = models.CharField(max_length=5)
+    file = models.FileField(upload_to=file_path)
+    entered_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        index = [
+            models.Index(fields=['ref_id', 'ref_field', 'ref_table'],
+                         name='ref_idx')
+        ]
--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# Model for Staff Information.
+class Forums(models.Model):
+    thread_id = models.AutoField(unique=True, primary_key=True)
+    thread_title = models.CharFiled(max_length=1000)
+    tags = models.SlugField(max_length=50)
+    is_closed = models.BooleanFiled(default=False)
+    created_by = models.BooleanFiled(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+# Model for thread discussion
+class Discussion(models.Model):
+    discussion_id = models.AutoField(unique=True, primary_key=True)
+    thread_id = models.ForeignKey('Forums', on_delete=models.CASCADE)
+    message = models.CharField(max_length=5000)
+    created_by = models.BooleanFiled(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        get_latest_by = ['created_date']
